94f5791f522fe6dc02781b4c868979983f299b79
Elian <43243730+ElianSarmiento@users.noreply.github.com>
2025-03-28 18:56:56 -0400

    Add files via upload

---
+++ Spring 2025 BootCamp work/Day 2/script.js
@@ -0,0 +1,9 @@
+document.getElementById("popup").addEventListener("click", function () {
+    alert("This is your pop-up message!");
+});
+
+
+document.querySelector(".popup-button").addEventListener("click", function () {
+    alert("You clicked the button!");
+});
+
